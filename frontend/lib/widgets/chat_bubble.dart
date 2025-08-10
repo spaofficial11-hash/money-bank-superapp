2f4b2fac0e068f7af335d6468ada547c343d02f0
spaofficial11-hash <spaofficial11@gmail.com>
2025-08-10 16:33:48 +0530

    Update chat_bubble.dart

--- frontend/lib/widgets/chat_bubble.dart
+++ frontend/lib/widgets/chat_bubble.dart
@@ -1,56 +1,31 @@
 import 'package:flutter/material.dart';
 
 class ChatBubble extends StatelessWidget {
-  final String message;
+  final String text;
   final bool isMe;
-  final DateTime timestamp;
 
   const ChatBubble({
     Key? key,
-    required this.message,
+    required this.text,
     required this.isMe,
-    required this.timestamp,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final alignment = isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start;
-    final bubbleColor = isMe ? Colors.blueAccent : Colors.grey[300];
-    final textColor = isMe ? Colors.white : Colors.black87;
-
-    return Column(
-      crossAxisAlignment: alignment,
-      children: [
-        Container(
-          margin: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 14),
-          constraints: BoxConstraints(
-            maxWidth: MediaQuery.of(context).size.width * 0.7,
-          ),
-          decoration: BoxDecoration(
-            color: bubbleColor,
-            borderRadius: BorderRadius.circular(12),
-          ),
-          child: Text(
-            message,
-            style: TextStyle(color: textColor, fontSize: 15),
-          ),
+    return Align(
+      alignment: isMe ? Alignment.centerRight : Alignment.centerLeft,
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 4),
+        padding: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: isMe ? Colors.blue : Colors.grey[300],
+          borderRadius: BorderRadius.circular(12),
         ),
-        Padding(
-          padding: const EdgeInsets.only(left: 8, right: 8),
-          child: Text(
-            _formatTime(timestamp),
-            style: const TextStyle(fontSize: 10, color: Colors.grey),
-          ),
+        child: Text(
+          text,
+          style: TextStyle(color: isMe ? Colors.white : Colors.black),
         ),
-      ],
+      ),
     );
-  }
-
-  String _formatTime(DateTime time) {
-    final hour = time.hour % 12 == 0 ? 12 : time.hour % 12;
-    final minute = time.minute.toString().padLeft(2, '0');
-    final amPm = time.hour >= 12 ? 'PM' : 'AM';
-    return '$hour:$minute $amPm';
   }
 }

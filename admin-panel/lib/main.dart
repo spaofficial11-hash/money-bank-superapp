bb08658b7db3e352beb94a66f0da7240cb739e5a
spaofficial11-hash <spaofficial11@gmail.com>
2025-08-10 09:20:56 +0530

    Create main.dart

---
+++ admin-panel/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'screens/dashboard.dart';
+
+void main() {
+  runApp(const AdminApp());
+}
+
+class AdminApp extends StatelessWidget {
+  const AdminApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Money Bank Admin',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const DashboardScreen(),
+    );
+  }
+}

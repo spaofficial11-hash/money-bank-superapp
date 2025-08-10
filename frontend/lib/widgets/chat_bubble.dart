6979963647b2d1e847b9b8cc8b3b2a3a447f8376
spaofficial11-hash <spaofficial11@gmail.com>
2025-08-10 17:12:28 +0530

    Update chat_bubble.dart

--- frontend/lib/widgets/chat_bubble.dart
+++ frontend/lib/widgets/chat_bubble.dart
@@ -3,11 +3,13 @@ import 'package:flutter/material.dart';
 class ChatBubble extends StatelessWidget {
   final String text;
   final bool isMe;
+  final DateTime? timestamp;
 
   const ChatBubble({
     Key? key,
     required this.text,
     required this.isMe,
+    this.timestamp,
   }) : super(key: key);
 
   @override
@@ -15,17 +17,39 @@ class ChatBubble extends StatelessWidget {
     return Align(
       alignment: isMe ? Alignment.centerRight : Alignment.centerLeft,
       child: Container(
-        margin: const EdgeInsets.symmetric(vertical: 4),
-        padding: const EdgeInsets.all(10),
+        margin: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 14),
         decoration: BoxDecoration(
-          color: isMe ? Colors.blue : Colors.grey[300],
-          borderRadius: BorderRadius.circular(12),
+          color: isMe ? Colors.blueAccent : Colors.grey.shade300,
+          borderRadius: BorderRadius.circular(16),
         ),
-        child: Text(
-          text,
-          style: TextStyle(color: isMe ? Colors.white : Colors.black),
+        child: Column(
+          crossAxisAlignment:
+              isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+          children: [
+            Text(
+              text,
+              style: TextStyle(
+                color: isMe ? Colors.white : Colors.black87,
+              ),
+            ),
+            if (timestamp != null)
+              Text(
+                _formatTime(timestamp!),
+                style: TextStyle(
+                  fontSize: 10,
+                  color: isMe ? Colors.white70 : Colors.black54,
+                ),
+              ),
+          ],
         ),
       ),
     );
+  }
+
+  String _formatTime(DateTime time) {
+    final hour = time.hour.toString().padLeft(2, '0');
+    final minute = time.minute.toString().padLeft(2, '0');
+    return "$hour:$minute";
   }
 }

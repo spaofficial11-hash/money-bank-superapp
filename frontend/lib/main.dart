65e0720abb243293c0ef1c0ab73107c3e285c93b
spaofficial11-hash <spaofficial11@gmail.com>
2025-08-11 09:49:22 +0530

    Update main.dart

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,38 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
-import 'package:money_bank/services/firebase_service.dart';
-import 'package:money_bank/screens/wallet_screen.dart';
-import 'package:money_bank/screens/admin_panel.dart';
-import 'package:money_bank/screens/device_verify.dart';
+import 'services/firebase_service.dart';
+import 'screens/wallet_screen.dart';
+import 'screens/admin_panel.dart';
+import 'screens/device_verify.dart';
 
-void main() {
-  runApp(
-    MultiProvider(
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MoneyBankApp());
+}
+
+class MoneyBankApp extends StatelessWidget {
+  const MoneyBankApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
       providers: [
         ChangeNotifierProvider<FirebaseService>(
           create: (_) => FirebaseService(),
         ),
       ],
-      child: const MoneyBankApp(),
-    ),
-  );
-}
-
-class MoneyBankApp extends StatelessWidget {
-  const MoneyBankApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Money Bank',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.blue),
-      initialRoute: '/',
-      routes: {
-        '/': (_) => const WalletScreen(),
-        '/admin': (_) => const AdminPanel(),
-        '/device-verify': (_) => const DeviceVerifyScreen(),
-      },
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Money Bank',
+        theme: ThemeData(primarySwatch: Colors.green),
+        initialRoute: '/',
+        routes: {
+          '/': (_) => const WalletScreen(),
+          '/admin': (_) => const AdminPanel(),
+          '/device-verify': (_) => const DeviceVerifyScreen(),
+        },
+      ),
     );
   }
 }
